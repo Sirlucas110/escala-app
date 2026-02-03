96db3f09c998e0c5611c60a0cfb937b0282cac99
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-03 10:07:42 -0400

    feat:

--- backend/configuracoes/settings.py
+++ backend/configuracoes/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'corsheaders',
+    'escala',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'escala',
     'simple_history',
 ]
 
